194bb02e282f41a64f0e54dae7f3105f8f3db398
igor_MB <106884907+IgorMarcoli@users.noreply.github.com>
2026-08-31 09:37:35 -0300

    Detectar automaticamente o host atual

--- app/Config/App.php
+++ app/Config/App.php
@@ -18,7 +18,14 @@ class App extends BaseConfig
      *    http://example.com/
      */
     public string $baseURL = '';
-
+    public function __construct()
+{
+    // detecta automaticamente o host atual (IP ou domínio)
+    if (isset($_SERVER['HTTP_HOST'])) {
+        $protocolo = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+        $this->baseURL = $protocolo . '://' . $_SERVER['HTTP_HOST'] . '/';
+    }
+}
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
      * If you want to accept multiple Hostnames, set this.
